bb1fd704825bc5c23437536117f204dd3527191c
paolanocom <paolanocom@gmail.com>
2024-02-06 17:57:00 +0800

    Debugging server deployment

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config({})
+require('dotenv').config()
 
 /* IMPORT MODELS */
 const Element = require('./models/Element');
